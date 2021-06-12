b7d04a196d999df44bd76ba5a00eb0e19b98a23b
Jason Anderson <jas.nanders.n@gmail.com>
2021-06-11 20:00:24 -0700

    built Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 function Header() {
+    return (
+        <div className="jumbotron">
+            <h1 className="display-4">Employee Directory</h1>
+            <p className="lead">Find an employee by their name in the search box below.</p>
+        </div>
+    )
 
 }
 
