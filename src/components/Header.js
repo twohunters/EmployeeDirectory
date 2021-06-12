32d152ca7feaf735636ca09abdd8f0428daf394f
Jason Anderson <jas.nanders.n@gmail.com>
2021-06-11 20:08:25 -0700

    built Search and modified Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <div className="jumbotron">
             <h1 className="display-4">Employee Directory</h1>
-            <p className="lead">Find an employee by their name in the search box below.</p>
+            <p className="lead">Search for an employee by their name in the search box below.</p>
         </div>
     )
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-function Search() {
-
+function Search(props) {
+    <form onSubmit={props.handleFormSubmit}>
+        <div className="form-group">
+            <input
+                onChange={props.handleInputChange}
+                value={props.search}
+                type="text"
+                className="form-control"
+                placeholder="First, Last"
+                id="search"
+            />
+        </div>
+    </form>
 }
 
 export default Search;
