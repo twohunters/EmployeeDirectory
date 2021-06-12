2d6ef50f66bdba8368f3e8d16552c17fb693342c
Jason Anderson <jas.nanders.n@gmail.com>
2021-06-11 21:54:52 -0700

    progress on Table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import Header from "./Header";
+import Search from "./Search";
+import API from "../utils/API";
 
-function Table(props) {
-    return
+class Table extends Component {
+    state = {
+        results: [],
+        resultsFiltered: [],
+        search: ""
+    };
+
+    componentDidMount() {
+        this.getEmployees();
+    };
+
+    getEmployees() {
+        API.getEmployees()
+            .then(res => {
+                this.setState({
+                    results: res.data.results,
+                    resultsFiltered: res.data.results
+                })
+            });
+    };
+
+    searchEmployees = (event) => {
+        event.preventDefault();
+
+        const search = this.state.search;
+        const list = this.state.results.filter(item => {
+            let value = Object.values(item).join("").toLowerCase();
+            return value.indexOf(search.toLowerCase()) !== -1;
+        });
+
+        this.setState({
+            resultsFiltered: list
+        });
+    };
+
+    handleInputChange = (event) => {
+        event.preventDefault();
+        this.setState({
+            search: event.target.value
+        });
+    };
+
+    sortEmployees = () => {
+        // create a way to sort a search for employees by first name, last name, dob, email
+    };
+
+    render = () => {
+        
+    };
 }
 
 export default Table;
