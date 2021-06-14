145a79fb8794a0dbf8cbc962481b70c12fd4b6b7
Jason Anderson <jas.nanders.n@gmail.com>
2021-06-13 21:24:20 -0700

    built Employees component

---
+++ src/components/Employees.js
@@ -0,0 +1,13 @@
+const Employees = (props) => {
+    return (
+        <tr key={props.index}>
+            <td><img src={props.picture.thumbnail} /></td>
+            <td>{props.name.first} {props.name.last}</td>
+            <td><a href={`mailto${props.email}`}>{this.props.email}</a></td>
+            <td>{props.phone}</td>
+            <td>{props.dob}</td>
+        </tr>
+    )
+}
+
+export default Employees
